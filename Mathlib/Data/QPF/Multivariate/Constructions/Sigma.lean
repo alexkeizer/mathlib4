250345baca0e4b3dcd0cf6b62468a147bf100fc1
Alex Keizer <alex@keizer.dev>
2023-03-02 21:03:04 +0100

    Show that Sigma preserves polynomiality

--- Mathlib/Data/QPF/Multivariate/Constructions/Sigma.lean
+++ Mathlib/Data/QPF/Multivariate/Constructions/Sigma.lean
@@ -50,64 +50,78 @@ namespace Sigma
 
 instance [∀ α, MvFunctor <| F α] : MvFunctor (Sigma F) where map := fun f ⟨a, x⟩ => ⟨a, f <$$> x⟩
 
-variable [q : ∀ α, MvQPF <| F α]
 
-/-- polynomial functor representation of a dependent sum -/
-protected def P : MvPFunctor n :=
-  ⟨Σ a, (P (F a)).A, fun x => (P (F x.1)).B x.2⟩
-set_option linter.uppercaseLean3 false in
-#align mvqpf.sigma.P MvQPF.Sigma.P
+section
+  variable [q : ∀ α, MvQPF <| F α]
 
-/-- abstraction function for dependent sums -/
-protected def abs ⦃α⦄ : (Sigma.P F).Obj α → Sigma F α
-  | ⟨a, f⟩ => ⟨a.1, MvQPF.abs ⟨a.2, f⟩⟩
-#align mvqpf.sigma.abs MvQPF.Sigma.abs
+  /-- polynomial functor representation of a dependent sum -/
+  protected def P : MvPFunctor n :=
+    ⟨Σ a, (P (F a)).A, fun x => (P (F x.1)).B x.2⟩
+  set_option linter.uppercaseLean3 false in
+  #align mvqpf.sigma.P MvQPF.Sigma.P
 
-/-- representation function for dependent sums -/
-protected def repr ⦃α⦄ : Sigma F α → (Sigma.P F).Obj α
-  | ⟨a, f⟩ =>
-    let x := MvQPF.repr f
-    ⟨⟨a, x.1⟩, x.2⟩
-#align mvqpf.sigma.repr MvQPF.Sigma.repr
+  /-- abstraction function for dependent sums -/
+  protected def abs ⦃α⦄ : (Sigma.P F).Obj α → Sigma F α
+    | ⟨a, f⟩ => ⟨a.1, MvQPF.abs ⟨a.2, f⟩⟩
+  #align mvqpf.sigma.abs MvQPF.Sigma.abs
 
-instance : MvQPF (Sigma F) where
-  P := Sigma.P F
-  abs {α} := @Sigma.abs _ _ F _ α
-  repr {α} := @Sigma.repr _ _ F _ α
-  abs_repr := by rintro α ⟨x, f⟩; simp only [Sigma.abs, Sigma.repr, Sigma.eta, abs_repr]
-  abs_map := by rintro α β f ⟨x, g⟩; simp only [Sigma.abs, MvPFunctor.map_eq]
-                simp only [(· <$$> ·), ← abs_map, ← MvPFunctor.map_eq]
+  /-- representation function for dependent sums -/
+  protected def repr ⦃α⦄ : Sigma F α → (Sigma.P F).Obj α
+    | ⟨a, f⟩ =>
+      let x := MvQPF.repr f
+      ⟨⟨a, x.1⟩, x.2⟩
+  #align mvqpf.sigma.repr MvQPF.Sigma.repr
+
+  instance : MvQPF (Sigma F) where
+    P := Sigma.P F
+    abs {α} := @Sigma.abs _ _ F _ α
+    repr {α} := @Sigma.repr _ _ F _ α
+    abs_repr := by rintro α ⟨x, f⟩; simp only [Sigma.abs, Sigma.repr, Sigma.eta, abs_repr]
+    abs_map := by rintro α β f ⟨x, g⟩; simp only [Sigma.abs, MvPFunctor.map_eq]
+                  simp only [(· <$$> ·), ← abs_map, ← MvPFunctor.map_eq]
+end
+
+instance [p : ∀ α, IsPolynomial <| F α] : IsPolynomial (Sigma F) where
+  repr_abs := by
+    intros α x
+    rcases x with ⟨a, f⟩
+    simp only [repr, abs, Sigma.repr, Sigma.abs]
+    rw [IsPolynomial.repr_abs]
+    rfl
 
 end Sigma
 
 namespace Pi
 
-instance [∀ α, MvFunctor <| F α] : MvFunctor (Pi F) where map f x a := f <$$> x a
+instance [∀ α, MvFunctor <| F α] : MvFunctor (Pi F) where
+  map f x a := f <$$> x a
 
-variable [∀ α, MvQPF <| F α]
+section
+  variable [∀ α, MvQPF <| F α]
 
-/-- polynomial functor representation of a dependent product -/
-protected def P : MvPFunctor n :=
-  ⟨∀ a, (P (F a)).A, fun x i => Σ a, (P (F a)).B (x a) i⟩
-set_option linter.uppercaseLean3 false in
-#align mvqpf.pi.P MvQPF.Pi.P
+  /-- polynomial functor representation of a dependent product -/
+  protected def P : MvPFunctor n :=
+    ⟨∀ a, (P (F a)).A, fun x i => Σ a, (P (F a)).B (x a) i⟩
+  set_option linter.uppercaseLean3 false in
+  #align mvqpf.pi.P MvQPF.Pi.P
 
-/-- abstraction function for dependent products -/
-protected def abs ⦃α⦄ : (Pi.P F).Obj α → Pi F α
-  | ⟨a, f⟩ => fun x => MvQPF.abs ⟨a x, fun i y => f i ⟨_, y⟩⟩
-#align mvqpf.pi.abs MvQPF.Pi.abs
+  /-- abstraction function for dependent products -/
+  protected def abs ⦃α⦄ : (Pi.P F).Obj α → Pi F α
+    | ⟨a, f⟩ => fun x => MvQPF.abs ⟨a x, fun i y => f i ⟨_, y⟩⟩
+  #align mvqpf.pi.abs MvQPF.Pi.abs
 
-/-- representation function for dependent products -/
-protected def repr ⦃α⦄ : Pi F α → (Pi.P F).Obj α
-  | f => ⟨fun a => (MvQPF.repr (f a)).1, fun _i a => (MvQPF.repr (f _)).2 _ a.2⟩
-#align mvqpf.pi.repr MvQPF.Pi.repr
+  /-- representation function for dependent products -/
+  protected def repr ⦃α⦄ : Pi F α → (Pi.P F).Obj α
+    | f => ⟨fun a => (MvQPF.repr (f a)).1, fun _i a => (MvQPF.repr (f _)).2 _ a.2⟩
+  #align mvqpf.pi.repr MvQPF.Pi.repr
 
-instance : MvQPF (Pi F) where
-  P := Pi.P F
-  abs := @Pi.abs _ _ F _
-  repr := @Pi.repr _ _ F _
-  abs_repr := by rintro α f; simp only [Pi.abs, Pi.repr, Sigma.eta, abs_repr]
-  abs_map := by rintro α β f ⟨x, g⟩; simp only [Pi.abs, (· <$$> ·), ← abs_map]; rfl
+  instance : MvQPF (Pi F) where
+    P := Pi.P F
+    abs := @Pi.abs _ _ F _
+    repr := @Pi.repr _ _ F _
+    abs_repr := by rintro α f; simp only [Pi.abs, Pi.repr, Sigma.eta, abs_repr]
+    abs_map := by rintro α β f ⟨x, g⟩; simp only [Pi.abs, (· <$$> ·), ← abs_map]; rfl
+end
 
 end Pi
 
